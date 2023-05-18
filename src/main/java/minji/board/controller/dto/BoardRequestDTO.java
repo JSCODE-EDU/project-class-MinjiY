8a55efd0a123ce78133d4d009d899223c1e8bef5
MinjiY <upi907@naver.com>
2023-05-18 20:06:21 +0900

    feat: title이 공백으로만 이루어 질 수 없도록 annotation 추가

--- src/main/java/minji/board/controller/dto/BoardRequestDTO.java
+++ src/main/java/minji/board/controller/dto/BoardRequestDTO.java
@@ -4,8 +4,12 @@ package minji.board.controller.dto;
 import lombok.Getter;
 import minji.board.model.BoardEntity;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @Getter
 public class BoardRequestDTO {
+    @NotBlank(message = "제목을 입력해주세요")
     public String title;
     public String content;
 
