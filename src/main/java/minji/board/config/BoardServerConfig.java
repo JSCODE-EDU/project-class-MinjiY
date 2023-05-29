665c39d076b1b61e38744141919f9765fb646221
MinjiY <upi907@naver.com>
2023-05-29 22:46:07 +0900

    feat: cors config 생성

---
+++ src/main/java/minji/board/config/BoardServerConfig.java
@@ -0,0 +1,20 @@
+package minji.board.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class BoardServerConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                ;
+    }
+
+}
